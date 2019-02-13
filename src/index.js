068f4fdb7fb85b6f63a401d97042caad28888d7d
Joeumar Souza <sjoeumar@lenovo.com>
2019-02-13 09:51:51 -0300

    Fixing router

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './views/layout/App';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import configureStore from "./state/store";
 
 const reduxStore = configureStore( {} );
@@ -10,7 +10,7 @@ const reduxStore = configureStore( {} );
 ReactDOM.render(
   <Provider store={reduxStore}>
     <Router>
-      <App />
+      <Route path='/' component={App} />
     </Router>
   </Provider>
   , document.getElementById('root'));
